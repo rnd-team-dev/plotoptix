9e52eaacce7c2a459cbf38d843221e929c3ca660
unknown <sulej.robert@gmail.com>
2019-05-06 12:33:30 +0200

    preserve input shape

--- plotoptix/utils.py
+++ plotoptix/utils.py
@@ -15,13 +15,22 @@ def map_to_colors(x: Any, cm_name: str) -> np.ndarray:
     Parameters
     ----------
     x : array_like
-        Input variable.
+        Input variable, array_like of any shape.
     cm_name : string
         Matplotlib colormap name.
+
+    Returns
+    -------
+    out : np.ndarray
+        Numpy array with RGB color values mapped from the input array values.
+        The output shape is x.shape + (3,).
     """
+    if x is None: raise ValueError()
+
     if not isinstance(x, np.ndarray): x = np.asarray(x)
 
     min_x = x.min()
     x = (1 / (x.max() - min_x)) * (x - min_x)
 
-    return cm.get_cmap(cm_name)(x)[:,:3] # skip alpha
+    c = cm.get_cmap(cm_name)(x)
+    return np.delete(c, np.s_[-1], len(c.shape) - 1) # skip alpha
