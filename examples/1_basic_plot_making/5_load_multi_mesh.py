eba99d652ea1248190bc8446fc3d195de55751a4
Robert Sulej <sulej.robert@gmail.com>
2020-05-29 15:32:48 +0200

    examples for alpha transparency in materials

---
+++ examples/1_basic_plot_making/5_load_multi_mesh.py
@@ -0,0 +1,46 @@
+"""
+Load multiple mesh from Wavefront .obj file, assign materials
+by mesh names.
+"""
+
+import numpy as np
+from plotoptix import TkOptiX
+from plotoptix.materials import m_diffuse, m_transparent_plastic, m_matt_plastic
+
+
+def main():
+
+    rt = TkOptiX() # create and configure, show the window later
+
+    rt.set_param(max_accumulation_frames=100)  # accumulate up to 100 frames
+    rt.set_background(0.99) # black background
+    rt.set_ambient(0.25) # some ambient light
+
+    m_diffuse["VarFloat3"] = { "base_color": [ 0.15, 0.17, 0.2 ] }
+    rt.update_material("diffuse", m_diffuse)
+
+    m_matt_plastic["VarFloat3"]["base_color"] = [ 0.5, 0.1, 0.05 ]
+    rt.setup_material("plastic", m_matt_plastic)
+
+    rt.load_texture("wing", "data/wing.png")
+
+    m_transparent_plastic["ColorTextures"] = [ "wing" ]
+    rt.setup_material("transparent", m_transparent_plastic)
+
+    rt.load_normal_tilt("transparent", "data/wing.png", prescale=0.002)
+
+    # another public-domain version with normals included:
+    materials = { "eye": "plastic", "wing": "transparent" }
+    rt.load_multiple_mesh_obj("data/fly.obj", materials)
+
+    # camera and light position auto-fit the scene geometry
+    rt.setup_camera("cam1")
+    d = np.linalg.norm(rt.get_camera_target() - rt.get_camera_eye())
+    rt.setup_light("light1", color=10, radius=0.3 * d)
+
+    rt.start()
+    print("done")
+
+
+if __name__ == '__main__':
+    main()
