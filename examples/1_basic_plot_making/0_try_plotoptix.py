bfa0421d3b71eafb8aaaff576bca65525f5efc11
Robert Sulej <sulej.robert@gmail.com>
2020-07-12 13:08:44 +0200

    tune mem usage on host side

--- examples/1_basic_plot_making/0_try_plotoptix.py
+++ examples/1_basic_plot_making/0_try_plotoptix.py
@@ -3,15 +3,18 @@ from plotoptix import TkOptiX
 
 
 def main():
-    optix = TkOptiX()
+    rt = TkOptiX()
 
     n = 1000000 # 1M data points
 
     xyz = 3 * (np.random.random((n, 3)) - 0.5)
     r = 0.02 * np.random.random(n) + 0.002
 
-    optix.set_data("plot", xyz, r=r)
-    optix.show() # note: non-blocking in the interactive shell
+    rt.set_data("plot", xyz, r=r)
+
+    rt.set_ambient(0.9); # set ambient light, brighter than default
+
+    rt.show() # note: non-blocking in the interactive shell
 
     print("done")
 
