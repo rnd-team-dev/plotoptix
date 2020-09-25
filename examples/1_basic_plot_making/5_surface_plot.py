900274006733a4f4517ca5468919fe74ea9cbc94
Robert Sulej <sulej.robert@gmail.com>
2020-09-25 22:59:26 +0200

    add wireframe example, more sample code tomorrow

--- examples/1_basic_plot_making/5_surface_plot.py
+++ examples/1_basic_plot_making/5_surface_plot.py
@@ -22,33 +22,41 @@ def main():
     X, Z = np.meshgrid(x, z)
     Y = f(X, Z)
 
-    optix = TkOptiX() # create and configure, show the window later
+    rt = TkOptiX() # create and configure, show the window later
 
-    optix.set_param(max_accumulation_frames=50)  # accumulate up to 50 frames
-    optix.set_background(0) # black background
-    optix.set_ambient(0.25) # some ambient light
+    rt.set_param(max_accumulation_frames=50)  # accumulate up to 50 frames
+    rt.set_background(0) # black background
+    rt.set_ambient(0.25) # some ambient light
 
 
     # try commenting out optional arguments
-    optix.set_data_2d("surface", Y,
-                      range_x=rx, range_z=rz,
-                      c=map_to_colors(Y, "OrRd"),
-                      floor_c=[0.05, 0.12, 0.38],
-                      floor_y=-1.75,
-                      make_normals=True
-                     )
+    rt.set_data_2d("surface", Y,
+                   range_x=rx, range_z=rz,
+                   c=map_to_colors(Y, "OrRd"),
+                   floor_c=[0.05, 0.12, 0.38],
+                   floor_y=-1.75,
+                   make_normals=True
+                  )
 
+    # add wireframe above the surface
+    rt.set_data_2d("wireframe", Y + 3,
+                   range_x=rx, range_z=rz,
+                   r=0.06 * np.abs(Y),
+                   geom="Graph",
+                   c=0.92
+                  )
 
     # set camera and light position to fit the scene
-    optix.setup_camera("cam1")
-    eye = optix.get_camera_eye()
+    rt.setup_camera("cam1")
+    eye = rt.get_camera_eye()
     eye[1] = 0.5 * eye[2]
-    optix.update_camera("cam1", eye=eye)
+    rt.update_camera("cam1", eye=eye)
 
-    d = np.linalg.norm(optix.get_camera_target() - eye)
-    optix.setup_light("light1", color=8, radius=0.3 * d)
+    d = np.linalg.norm(rt.get_camera_target() - eye)
+    rt.setup_light("light1", color=8, radius=0.3 * d)
 
-    optix.start()
+    rt.start()
+
     print("done")
 
 
