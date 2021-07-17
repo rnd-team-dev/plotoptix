d78ad29ecfc9cd2ea8c9ba0b362963970b1eaf07
Robert Sulej <sulej.robert@gmail.com>
2021-07-17 14:53:23 +0200

    update url's

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ except ImportError:
 
 setup(name='plotoptix',
       version='0.14.0',
-      url='https://rnd.team/project/plotoptix',
+      url='https://rnd.team/plotoptix',
       project_urls={
           'Documentation': 'https://plotoptix.rnd.team',
           'Examples': 'https://github.com/rnd-team-dev/plotoptix/tree/master/examples',
