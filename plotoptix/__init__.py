3204fa85188d53ffd121495cadf304df7dff70d2
Robert Sulej <sulej.robert@gmail.com>
2023-08-18 19:30:15 +0200

    prepare for the release

--- plotoptix/__init__.py
+++ plotoptix/__init__.py
@@ -13,7 +13,7 @@ __all__ = ["enums", "materials", "utils", "npoptix", "tkoptix"]
 __author__  = "Robert Sulej, R&D Team <dev@rnd.team>"
 __status__  = "beta"
 __version__ = "0.17.0"
-__date__    = "16 July 2023"
+__date__    = "18 August 2023"
 
 import logging
 
