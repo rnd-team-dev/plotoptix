8e43851f1b9cdbbbdb4832f92c6bb43490fd319f
Robert Sulej <sulej.robert@gmail.com>
2026-06-01 12:30:38 +0200

    fix ffmpeg not found on sandboxed python in windows

--- plotoptix/__init__.py
+++ plotoptix/__init__.py
@@ -1,5 +1,5 @@
 """
-3D ray tracing package for Python based on NVIDIA OptiX 7 framework,
+3D ray tracing package for Python based on NVIDIA OptiX framework,
 wrapped in RnD.SharpOptiX C#/C++/CUDA libraries by R&D Team.
 
 https://github.com/rnd-team-dev/plotoptix/blob/master/LICENSE.txt
@@ -12,12 +12,53 @@ __all__ = ["enums", "materials", "utils", "npoptix", "tkoptix"]
 
 __author__  = "Robert Sulej, R&D Team <dev@rnd.team>"
 __status__  = "beta"
-__version__ = "0.19.1"
-__date__    = "28 May 2026"
+__version__ = "0.19.2"
+__date__    = "1 June 2026"
 
 import logging
 
 logging.basicConfig(level=logging.WARN, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+
+import os
+import sys
+import ctypes
+
+def _bootstrap_ffmpeg_dlls():
+    if sys.platform != "win32" or sys.version_info < (3, 8):
+        return
+
+    # Shared FFmpeg DLL names to search for:
+    target_dlls = ["avcodec-62.dll", "avformat-62.dll", "avutil-60.dll"]
+    ffmpeg_dir = None
+
+    path_env = os.environ.get("PATH", "")
+    for directory in path_env.split(os.pathsep):
+        clean_dir = directory.strip('"')
+        if not os.path.isdir(clean_dir):
+            continue
+        
+        if all(os.path.isfile(os.path.join(clean_dir, dll)) for dll in target_dlls):
+            ffmpeg_dir = clean_dir
+            break
+
+    if not ffmpeg_dir:
+        print(f"[{__name__}] Warning: No FFmpeg DLLs found in PATH.")
+        return
+
+    try:
+        _ = os.add_dll_directory(ffmpeg_dir)
+        
+        for dll in target_dlls:
+            dll_path = os.path.join(ffmpeg_dir, dll)
+            if os.path.isfile(dll_path):
+                # Using ALTER_RE_SEARCH_PATH helps resolve dependencies in the same folder
+                ctypes.CDLL(dll_path, winmode=0x00000008) 
+                
+    except Exception as e:
+        # Sandbox denied read permissions entirely
+        print(f"[{__name__}] Warning: Failed to map FFmpeg DLL directory: {e}", sys.stderr)
+
+_bootstrap_ffmpeg_dlls()
 
 # import PlotOptiX modules ###############################################
 from plotoptix.enums import *
@@ -40,9 +81,9 @@ try:
 
     if version.parse(__version__) < version.parse(versions[-1]):
         print(80 * "*")
-        print("PlotOptiX newer version is available:", versions[-1])
-        print("to update your release use:")
-        print("      pip install plotoptix --upgrade")
+        print(f"[{__name__}] PlotOptiX newer version is available:", versions[-1])
+        print(f"[{__name__}] to update your release use:")
+        print(f"[{__name__}]       pip install plotoptix --upgrade")
         print(80 * "*")
 
 except: pass
